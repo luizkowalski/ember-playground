6f9f608f9e6f4e2cbe6394f377a3e788526d7519
Luiz Eduardo <luizeduardokowalski@gmail.com>
2016-08-04 01:49:08 +0200

    More WIP

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  session: Ember.inject.service('session'),
   model(){
     console.log(this.get('login'));
   }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  session: Ember.inject.service('session'),
   actions: {
     login() {
       var controller = this.controller
