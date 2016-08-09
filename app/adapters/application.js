c2f9fa8d9a1722e13bd7735daa6d651585e52987
Luiz Eduardo <luizeduardokowalski@gmail.com>
2016-08-09 22:14:49 +0200

    WIP

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,7 +5,7 @@ export default DS.RESTAdapter.extend({
   host: 'http://127.0.0.1:8080/v1',
   headers: Ember.computed('session.data.authenticated.currentUser.backendAccessToken', function() {
     return {
-      "Auth-Token": this.get("session.data.authenticated.currentUser.backendAccessToken"),
+      "Auth-Token": this.get("session.data.authenticated.currentUser.backendAccessToken")
     };
   })
 });
--- app/components/chat-room.js
+++ app/components/chat-room.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   actions: {
     postMessage: function(params) {
-      console.log(this.get('uid'));
-      console.log(this.get('content'));
-
+      var token = this.get('token');
+      var content = this.get('content');
+      var uid = this.get('room.uid');
+      Ember.$.ajax({
+         type: "POST",
+         contentType: "application/json",
+         url: "http://127.0.0.1:8080/v1/rooms/"+uid+"/messages/new",
+         headers: {
+           'Auth-Token': token
+         },
+         data: JSON.stringify({ content: content })
+       });
       this.set('content', '');
     }
   }
